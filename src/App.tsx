82cc066ecbaa9a0f2d36f03f81c290ba4faf40d0
Danil Beltsov <danilbeltsov@gmail.com>
2020-10-02 11:02:03 +0300

    Fix i18n loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,32 @@
 import { Footer, Header, Router } from '@components/core';
 import { store } from '@store';
-import React from 'react';
+import React, { Suspense } from 'react';
 import { hot } from 'react-hot-loader/root';
 import { connect, Provider } from 'react-redux';
 import './App.scss';
 
 function App() {
-	return (
-		<Provider store={store}>
-			<WrappedMain />
-		</Provider>
-	);
+  return (
+    <Provider store={store}>
+      <Suspense fallback={null}>
+        <WrappedMain />
+      </Suspense>
+    </Provider>
+  );
 }
 
 function Main() {
-	return (
-		<>
-			<div className="main-wrapper">
-				<Header />
-				<main className="router-context">
-					<Router />
-				</main>
-			</div>
-			<Footer />
-		</>
-	);
+  return (
+    <>
+      <div className="main-wrapper">
+        <Header />
+        <main className="router-context">
+          <Router />
+        </main>
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export const WrappedMain = connect()(Main);
--- src/i18n.ts
+++ src/i18n.ts
@@ -14,9 +14,6 @@ i18n
       loadPath: `./locale/{{lng}}.json`,
     },
     keySeparator: ':',
-    react: {
-      useSuspense: false,
-    },
   });
 
 export default i18n;
