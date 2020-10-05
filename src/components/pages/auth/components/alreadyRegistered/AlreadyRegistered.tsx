c2cfcf3463cb061cd48be41b06332fcd71bce2a4
ekuznetski <e.kuznetski@gmail.com>
2020-10-05 15:47:46 +0300

    [auth] translate already registered component

--- src/components/pages/auth/components/alreadyRegistered/AlreadyRegistered.tsx
+++ src/components/pages/auth/components/alreadyRegistered/AlreadyRegistered.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './AlreadyRegistered.scss';
+import { LocaleLink } from '@components/shared';
+import { useTranslation } from 'react-i18next';
 
 export function AlreadyRegistered() {
+  const { t } = useTranslation();
   return (
-    <div className="mt-5 text-center auth-under-form fadeFromBottom-row__6">
-      Already Registered?
-      <Link className="already__link ml-1" to={`/login`}>
-        Sign In
-      </Link>
+    <div className="mt-5 text-center under-form">
+      {t('Already Registered?')}
+      <LocaleLink className="already__link ml-1" to="/login">
+        {t('Sign In')}
+      </LocaleLink>
     </div>
   );
 }
