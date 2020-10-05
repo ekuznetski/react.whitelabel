278d9846571ea08328c2153430aa1c2564a35ab3
ekuznetski <e.kuznetski@gmail.com>
2020-10-05 20:41:49 +0300

    [registration] small fix

--- src/components/pages/auth/components/alreadyRegistered/AlreadyRegistered.tsx
+++ src/components/pages/auth/components/alreadyRegistered/AlreadyRegistered.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import './AlreadyRegistered.scss';
 import { LocaleLink } from '@components/shared';
 import { useTranslation } from 'react-i18next';
+import classNames from 'classnames';
 
-export function AlreadyRegistered() {
+export function AlreadyRegistered({ className }: any) {
   const { t } = useTranslation();
   return (
-    <div className="mt-5 text-center under-form">
+    <div className={classNames('mt-5 text-center under-form', className)}>
       {t('Already Registered?')}
       <LocaleLink className="already__link ml-1" to="/login">
         {t('Sign In')}
