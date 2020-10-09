e41947e533b1c72ab506f252e07a4b93c9fdbf88
MisaGu <aslebed@yahoo.com>
2020-10-09 17:58:44 +0300

    [sage] add generator to sage Middleware

--- src/store/sagas.ts
+++ src/store/sagas.ts
@@ -47,137 +47,101 @@ import {
 import { EActionTypes } from './store.enum';
 import { IAction } from './store.interface';
 
-function* getContentMiddleware({ payload }: IAction) {
-  try {
-    const response = yield call(getContentRequest, payload?.page);
-    yield put(ac_requestActionSuccess({ requestActionType: EActionTypes.fetchContent }));
-    yield put(ac_saveContent({ [payload?.page]: response.data }));
-  } catch (e) {
-    yield put(ac_requestActionFailure({ requestActionType: EActionTypes.fetchContent }));
-  }
+function __store() {
+  return store.getState();
 }
 
-function* getProfileMiddleware() {
-  try {
-    const { response }: IClientProfileResponse = yield call(getProfileRequest);
-    yield put(ac_saveProfile(response.message));
-    yield put(ac_requestActionSuccess({ requestActionType: EActionTypes.fetchProfile }));
-  } catch (e) {
-    yield put(ac_requestActionFailure({ requestActionType: EActionTypes.fetchProfile }));
-  }
+function $$(fn: any, actionType: EActionTypes, storeValue: any = null) {
+  return function* (action?: IAction) {
+    const { payload, force, onSuccess, onFailure } = action || {};
+
+    if (force || !storeValue) {
+      try {
+        let response = yield fn(action);
+        console.log(response);
+        if (onSuccess) yield call(onSuccess, response);
+        yield put(ac_requestActionSuccess({ requestActionType: actionType }));
+      } catch (e) {
+        if (onFailure) yield call(onFailure, e);
+        yield put(ac_requestActionFailure({ requestActionType: actionType }));
+      }
+    } else {
+      yield put(ac_requestActionSuccess({ requestActionType: EActionTypes.fetchProfile }));
+    }
+  };
 }
 
-function* loginMiddleware({ payload, onSuccess, onFailure }: IAction) {
-  try {
-    const { response }: ILoginResponse = yield call(loginRequest, payload);
-    yield put(ac_saveProfile(response.profile));
-    if (onSuccess) yield call(onSuccess, response);
-    yield put(ac_requestActionSuccess({ requestActionType: EActionTypes.login }));
-  } catch (e) {
-    if (onFailure) yield call(onFailure, e);
-    yield put(ac_requestActionFailure({ requestActionType: EActionTypes.login }));
-  }
+function* mw_getContent({ payload }: IAction) {
+  const response = yield call(getContentRequest, payload?.page);
+  yield put(ac_saveContent({ [payload?.page]: response.data }));
+  return response;
 }
 
-function* logoutMiddleware() {
-  try {
-    const { response }: any = yield call(logoutRequest);
-    yield put(ac_clearStore());
-    console.log('store cleared');
-    yield put(ac_requestActionSuccess({ requestActionType: EActionTypes.logout }));
-  } catch (e) {
-    yield put(ac_requestActionFailure({ requestActionType: EActionTypes.logout }));
-  }
+function* mw_getProfile() {
+  const { response }: IClientProfileResponse = yield call(getProfileRequest);
+  yield put(ac_saveProfile(response.message));
+  return response;
 }
 
-function* userExistMiddleware({ payload, onSuccess, onFailure }: IAction) {
-  try {
-    const { response }: IBaseResponse = yield call(userExistsRequest, payload);
-    yield put(ac_requestActionSuccess({ requestActionType: EActionTypes.userExists }));
-    if (onSuccess) yield call(onSuccess, response);
-  } catch (e) {
-    if (onFailure) yield call(onFailure, e);
-    yield put(ac_requestActionFailure({ requestActionType: EActionTypes.userExists }));
-  }
+function* mw_login({ payload }: IAction) {
+  const { response }: ILoginResponse = yield call(loginRequest, payload);
+  yield put(ac_saveProfile(response.profile));
+  return response;
 }
 
-function* clientAddMiddleware({ payload, onSuccess, onFailure }: IAction) {
-  try {
-    const { response }: IClientAddResponse = yield call(clientAddRequest, payload);
-    yield put(ac_requestActionSuccess({ requestActionType: EActionTypes.preRegister }));
-    if (onSuccess) yield call(onSuccess, response);
-  } catch (e) {
-    if (onFailure) yield call(onFailure, e);
-    yield put(ac_requestActionFailure({ requestActionType: EActionTypes.preRegister }));
-  }
+function* mw_logout() {
+  const { response }: any = yield call(logoutRequest);
+  yield put(ac_clearStore());
+  console.log('store cleared');
+  return response;
 }
 
-function* forgotPasswordMiddleware({ payload, onSuccess, onFailure }: IAction) {
-  try {
-    const { response } = yield call(forgotPasswordRequest, payload);
-    yield put(ac_requestActionSuccess({ requestActionType: EActionTypes.forgotPassword }));
-    if (onSuccess) yield call(onSuccess, response);
-  } catch (e) {
-    if (onFailure) yield call(onFailure, e);
-    yield put(ac_requestActionFailure({ requestActionType: EActionTypes.forgotPassword }));
-  }
+function* mw_userExist({ payload }: IAction) {
+  const { response }: IBaseResponse = yield call(userExistsRequest, payload);
+  return response;
 }
 
-function* resetPasswordMiddleware({ payload, onSuccess, onFailure }: IAction) {
-  try {
-    const { response } = yield call(resetPasswordRequest, payload);
-    yield put(ac_requestActionSuccess({ requestActionType: EActionTypes.resetPassword }));
-    if (onSuccess) yield call(onSuccess, response);
-  } catch (e) {
-    if (onFailure) yield call(onFailure, e);
-    yield put(ac_requestActionFailure({ requestActionType: EActionTypes.resetPassword }));
-  }
+function* mw_clientAdd({ payload }: IAction) {
+  const { response }: IClientAddResponse = yield call(clientAddRequest, payload);
+  return response;
 }
 
-function* setProfileMiddleware({ payload, onSuccess, onFailure }: IAction) {
-  try {
-    const { response }: ISetProfileResponse = yield call(clientSetProfileRequest, payload);
-    yield put(ac_saveProfile(response.profile));
-    yield put(ac_requestActionSuccess({ requestActionType: EActionTypes.register }));
-    if (onSuccess) yield call(onSuccess, response);
-  } catch (e) {
-    if (onFailure) yield call(onFailure, e);
-    yield put(ac_requestActionFailure({ requestActionType: EActionTypes.register }));
-  }
+function* mw_forgotPassword({ payload }: IAction) {
+  const { response } = yield call(forgotPasswordRequest, payload);
+  return response;
 }
 
-function* getGeoIPMiddleware() {
-  try {
-    const response = yield call(getGeoIpRequest);
-    yield put(ac_saveGeoIpData(response));
-    yield put(ac_requestActionSuccess({ requestActionType: EActionTypes.fetchGeoIpData }));
-  } catch (e) {
-    yield put(ac_requestActionFailure({ requestActionType: EActionTypes.fetchGeoIpData }));
-  }
+function* mw_resetPassword({ payload }: IAction) {
+  const { response } = yield call(resetPasswordRequest, payload);
+  return response;
 }
 
-function* getTradingAccountsMiddleware() {
-  try {
-    const { response }: ITradingAccountsResponse = yield call(tradingAccountsRequest);
-    yield put(ac_saveTradingAccounts(new MClientTradingData(response)));
-    yield put(ac_requestActionSuccess({ requestActionType: EActionTypes.fetchTradingAccounts }));
-  } catch (e) {
-    yield put(ac_requestActionFailure({ requestActionType: EActionTypes.fetchTradingAccounts }));
-  }
+function* mw_setProfile({ payload, onSuccess, onFailure }: IAction) {
+  const { response }: ISetProfileResponse = yield call(clientSetProfileRequest, payload);
+  yield put(ac_saveProfile(response.profile));
+  return response;
 }
 
-function* getWithdrawHistoryMiddleware() {
-  try {
-    const { response }: IWithdrawalHistoryResponse = yield call(withdrawalsHistoryRequest);
-    const payload = response.message.map((item) => new MWithdrawalHistoryItem(item));
-    yield put(ac_saveWithdrawHistory(payload));
-    yield put(ac_requestActionSuccess({ requestActionType: EActionTypes.fetchWithdrawHistory }));
-  } catch (e) {
-    yield put(ac_requestActionFailure({ requestActionType: EActionTypes.fetchWithdrawHistory }));
-  }
+function* mw_getGeoIP() {
+  const response = yield call(getGeoIpRequest);
+  yield put(ac_saveGeoIpData(response));
+  return response;
 }
 
-function* getWithdrawLimitMiddleware({ payload }: IAction<{ accountId: number; platform: string }>) {
+function* mw_getTradingAccounts() {
+  const { response }: ITradingAccountsResponse = yield call(tradingAccountsRequest);
+  yield put(ac_saveTradingAccounts(new MClientTradingData(response)));
+  return response;
+}
+
+function* mw_getWithdrawHistory() {
+  const { response }: IWithdrawalHistoryResponse = yield call(withdrawalsHistoryRequest);
+  const payload = response.message.map((item) => new MWithdrawalHistoryItem(item));
+  yield put(ac_saveWithdrawHistory(payload));
+  return response;
+}
+
+function* mw_getWithdrawLimit({ payload }: IAction<{ accountId: number; platform: string }>) {
   try {
     const { response }: IWithdrawalLimitResponse = yield call(withdrawalsLimitRequest, {
       trade_account: payload?.accountId,
@@ -188,109 +152,99 @@ function* getWithdrawLimitMiddleware({ payload }: IAction<{ accountId: number; p
   } catch (e) {
     // if can't load the actual limit send the account balance
     const limit =
-      store.getState().data?.tradingData?.accounts.find((account) => account.accountId === payload?.accountId)
-        ?.balance || 1000000000000;
+      __store().data?.tradingData?.accounts.find((account) => account.accountId === payload?.accountId)?.balance ||
+      1000000000000;
     yield put(ac_saveWithdrawLimit({ limit }));
     yield put(ac_requestActionFailure({ requestActionType: EActionTypes.fetchWithdrawLimit }));
   }
 }
 
-function* getClientStatusDataMiddleware() {
-  try {
-    const { response }: IClientStatusDataResponse = yield call(getClientDataRequest);
-    const payload = new MClientData(response);
-    yield put(ac_saveClientData(payload));
-    yield put(ac_requestActionSuccess({ requestActionType: EActionTypes.fetchClientData }));
-  } catch (e) {
-    yield put(ac_requestActionFailure({ requestActionType: EActionTypes.fetchClientData }));
-  }
+function* mw_getClientStatusData() {
+  const { response }: IClientStatusDataResponse = yield call(getClientDataRequest);
+  const payload = new MClientData(response);
+  yield put(ac_saveClientData(payload));
+  return response;
 }
 
-function* makeInternalTransferMiddleware({ payload, onSuccess, onFailure }: IAction) {
-  try {
-    yield call(internalTransferRequest, payload);
-    yield put(ac_fetchTradingAccounts());
-    yield put(ac_requestActionSuccess({ requestActionType: EActionTypes.makeInternalTransfer }));
-    if (onSuccess) yield call(onSuccess);
-  } catch (e) {
-    if (onFailure) yield call(onFailure, e);
-    yield put(ac_requestActionFailure({ requestActionType: EActionTypes.makeInternalTransfer }));
-  }
+function* mw_makeInternalTransfer({ payload, onSuccess, onFailure }: IAction) {
+  const { response }: any = yield call(internalTransferRequest, payload);
+  yield put(ac_fetchTradingAccounts());
+  return response;
 }
 
-function* fetchTransactionalStatementsMiddleware({ payload, onSuccess, onFailure }: IAction) {
-  try {
-    const { response }: ITransactionalStatementsResponse = yield call(getTransactionalStatementsRequest, payload);
-    const data = new MTransactionalStatementData(response);
-    yield put(ac_saveTransactionalStatements(data));
-    yield put(ac_requestActionSuccess({ requestActionType: EActionTypes.fetchTransactionalStatements }));
-    if (onSuccess) yield call(onSuccess);
-  } catch (e) {
-    if (onFailure) yield call(onFailure, e);
-    yield put(ac_requestActionFailure({ requestActionType: EActionTypes.fetchTransactionalStatements }));
-  }
+function* mw_fetchTransactionalStatements({ payload, onSuccess, onFailure }: IAction) {
+  const { response }: ITransactionalStatementsResponse = yield call(getTransactionalStatementsRequest, payload);
+  const data = new MTransactionalStatementData(response);
+  yield put(ac_saveTransactionalStatements(data));
+  return response;
 }
 
 export function* getContentSaga() {
-  yield takeEvery(EActionTypes.fetchContent, getContentMiddleware);
+  yield takeEvery(EActionTypes.fetchContent, $$(mw_getContent, EActionTypes.fetchContent));
 }
 
 export function* getGeoIPSaga() {
-  yield takeEvery(EActionTypes.fetchGeoIpData, getGeoIPMiddleware);
+  yield takeEvery(EActionTypes.fetchGeoIpData, $$(mw_getGeoIP, EActionTypes.fetchGeoIpData));
 }
 
 export function* getProfileSaga() {
-  yield takeEvery(EActionTypes.fetchProfile, getProfileMiddleware);
+  yield takeEvery(
+    EActionTypes.fetchProfile,
+    $$(mw_getProfile, EActionTypes.fetchProfile, __store().data.client.profile),
+  );
 }
 
 export function* loginSaga() {
-  yield takeEvery(EActionTypes.login, loginMiddleware);
+  yield takeEvery(EActionTypes.login, $$(mw_login, EActionTypes.login));
 }
 
 export function* logoutSaga() {
-  yield takeEvery(EActionTypes.logout, logoutMiddleware);
+  yield takeEvery(EActionTypes.logout, $$(mw_logout, EActionTypes.logout));
 }
 
 export function* clientAddSaga() {
-  yield takeEvery(EActionTypes.preRegister, clientAddMiddleware);
+  yield takeEvery(EActionTypes.preRegister, $$(mw_clientAdd, EActionTypes.preRegister));
 }
 
 export function* userExistSaga() {
-  yield takeEvery(EActionTypes.userExists, userExistMiddleware);
+  yield takeEvery(EActionTypes.userExists, $$(mw_userExist, EActionTypes.userExists));
 }
 
 export function* forgotPasswordSaga() {
-  yield takeEvery(EActionTypes.forgotPassword, forgotPasswordMiddleware);
+  yield takeEvery(EActionTypes.forgotPassword, $$(mw_forgotPassword, EActionTypes.forgotPassword));
 }
 
 export function* resetPasswordSaga() {
-  yield takeEvery(EActionTypes.resetPassword, resetPasswordMiddleware);
+  yield takeEvery(EActionTypes.resetPassword, $$(mw_resetPassword, EActionTypes.resetPassword));
 }
 
 export function* setProfileSaga() {
-  yield takeEvery(EActionTypes.register, setProfileMiddleware);
+  yield takeEvery(EActionTypes.register, $$(mw_setProfile, EActionTypes.register));
 }
 
 export function* getWithdrawHistorySaga() {
-  yield takeEvery(EActionTypes.fetchWithdrawHistory, getWithdrawHistoryMiddleware);
+  yield takeEvery(EActionTypes.fetchWithdrawHistory, $$(mw_getWithdrawHistory, EActionTypes.fetchWithdrawHistory));
 }
 
 export function* getWithdrawLimitSaga() {
-  yield takeEvery(EActionTypes.fetchWithdrawLimit, getWithdrawLimitMiddleware);
+  yield takeEvery(EActionTypes.fetchWithdrawLimit, mw_getWithdrawLimit);
 }
 
 export function* getClientStatusDataSaga() {
-  yield takeEvery(EActionTypes.fetchClientData, getClientStatusDataMiddleware);
+  yield takeEvery(EActionTypes.fetchClientData, $$(mw_getClientStatusData, EActionTypes.fetchClientData));
 }
 
 export function* getTradingAccountsSage() {
-  yield takeEvery(EActionTypes.fetchTradingAccounts, getTradingAccountsMiddleware);
+  yield takeEvery(EActionTypes.fetchTradingAccounts, $$(mw_getTradingAccounts, EActionTypes.fetchTradingAccounts));
 }
 
 export function* makeInternalTransferSage() {
-  yield takeEvery(EActionTypes.makeInternalTransfer, makeInternalTransferMiddleware);
+  yield takeEvery(EActionTypes.makeInternalTransfer, $$(mw_makeInternalTransfer, EActionTypes.makeInternalTransfer));
 }
 
 export function* fetchTransactionalStatementsSage() {
-  yield takeEvery(EActionTypes.fetchTransactionalStatements, fetchTransactionalStatementsMiddleware);
+  yield takeEvery(
+    EActionTypes.fetchTransactionalStatements,
+    $$(mw_fetchTransactionalStatements, EActionTypes.fetchTransactionalStatements),
+  );
 }
