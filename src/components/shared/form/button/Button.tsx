9c413c75b07fef09f0d113d66a6bf0fe0e511788
MisaGu <aslebed@yahoo.com>
2020-10-13 16:47:27 +0300

    [button] add loading on the Button component

--- src/components/shared/form/button/Button.tsx
+++ src/components/shared/form/button/Button.tsx
@@ -1,32 +1,50 @@
+import { Svg } from '@components/shared';
 import { EFormStatus } from '@domain/enums';
+import { EActionTypes, IStore } from '@store';
 import classNames from 'classnames';
 import { useFormikContext } from 'formik';
 import React, { forwardRef, memo } from 'react';
+import { useSelector } from 'react-redux';
 import './Button.scss';
 
 interface IButton extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-	children: React.ReactNode;
-	checkFormValidity?: boolean;
+  children: React.ReactNode;
+  checkFormValidity?: boolean;
+  loadingOnAction?: EActionTypes | EActionTypes[];
+  isLoading?: boolean;
 }
 
 export const Button = memo(
-	forwardRef<HTMLButtonElement, IButton>(function Button({ className = '', checkFormValidity = false, ...props }, ref) {
-		const formikProps = useFormikContext();
-		const _disabled =
-			props.disabled ||
-			(formikProps && (formikProps?.status === EFormStatus.disabled || (checkFormValidity && !formikProps?.isValid)));
-		const _buttonProps = { ...props };
-		delete _buttonProps.children;
+  forwardRef<HTMLButtonElement, IButton>(function Button({ className = '', checkFormValidity = false, ...props }, ref) {
+    const { activeRequestsList } = useSelector<IStore, { activeRequestsList: EActionTypes[] }>((state) => ({
+      activeRequestsList: state.app.requests.activeList,
+    }));
+    const formikProps = useFormikContext();
 
-		return (
-			<button
-				className={classNames('common-button', className, _disabled && 'disabled')}
-				{..._buttonProps}
-				disabled={_disabled}
-				ref={ref}
-			>
-				{props.children}
-			</button>
-		);
-	}),
+    const _loading =
+      props.isLoading ||
+      (props.loadingOnAction &&
+        !![props.loadingOnAction].flat().find((action) => activeRequestsList.includes(action as EActionTypes)));
+    const _disabled =
+      props.disabled ||
+      (formikProps && (formikProps?.status === EFormStatus.disabled || (checkFormValidity && !formikProps?.isValid)));
+    const _buttonProps = { ...props };
+    delete _buttonProps.children;
+    delete _buttonProps.isLoading;
+    delete _buttonProps.loadingOnAction;
+
+    return (
+      <button
+        className={classNames('common-button', className, (_disabled || _loading) && 'disabled')}
+        {..._buttonProps}
+        disabled={_disabled || _loading}
+        ref={ref}
+      >
+        <div className="context">
+          {_loading && <Svg href="loading.svg" width={20} height={20} className="loader" />}
+          {props.children}
+        </div>
+      </button>
+    );
+  }),
 );
