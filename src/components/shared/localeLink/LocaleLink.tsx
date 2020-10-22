b3723659dc73f1296c633865fabef0d67bd5fec9
ekuznetski <e.kuznetski@gmail.com>
2020-10-22 14:25:57 +0300

    [LocaleLink] add onClick handler

--- src/components/shared/localeLink/LocaleLink.tsx
+++ src/components/shared/localeLink/LocaleLink.tsx
@@ -1,10 +1,11 @@
 import { IAppStore, IStore } from '@store';
-import React, { memo } from 'react';
+import React, { memo, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { Link, LinkProps } from 'react-router-dom';
 
 type ILocaleLink = {
   children: React.ReactNode;
+  onClick?: (...args: any[]) => void;
 } & LinkProps;
 
 export const LocaleLink = memo(function LocaleLink(props: ILocaleLink) {
@@ -26,6 +27,10 @@ export const LocaleLink = memo(function LocaleLink(props: ILocaleLink) {
   function formatPath(path: string) {
     return `${locale && locale.length ? '/' + locale : ''}${path.replace(/^\\?(S+)/, '/$1')}`;
   }
+
+  useEffect(() => {
+    props?.onClick?.();
+  }, []);
 
   return (
     <Link {...navProps} to={_to}>
