48cf9b84b4713d71c7e2d07829dc2dfd6db438c1
MisaGu <aslebed@yahoo.com>
2020-12-09 14:43:47 +0200

    [general] render optimization

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { env } from '@env';
 import { ac_updateRouteParams, IStore, store } from '@store';
 import { useDeviceDetect } from '@utils/hooks';
 import classNames from 'classnames';
-import React, { Suspense, useEffect } from 'react';
+import React, { Suspense, useEffect, useMemo } from 'react';
 import { browserName, osName } from 'react-device-detect';
 import TagManager from 'react-gtm-module';
 import { hot } from 'react-hot-loader/root';
@@ -62,28 +62,30 @@ export function Main() {
     }
   }, [section]);
 
-  return (
-    <>
-      <div
-        className={classNames(
-          'main-wrapper',
-          env.LABEL?.toLowerCase(),
-          osName.toLowerCase(),
-          browserName.toLowerCase().replace(/mobile|\s/g, ''),
-          device.isMobile && 'isMobile',
-          device.isTablet && 'isTablet',
-          device.isDesktop && 'isDesktop',
-        )}
-      >
-        <Header />
-        <main className="router-context">
-          <Router />
-        </main>
-      </div>
-      <Footer />
-      <div id="dynamic-portals" />
-    </>
-  );
+  return useMemo(() => {
+    return (
+      <>
+        <div
+          className={classNames(
+            'main-wrapper',
+            env.LABEL?.toLowerCase(),
+            osName.toLowerCase(),
+            browserName.toLowerCase().replace(/mobile|\s/g, ''),
+            device.isMobile && 'isMobile',
+            device.isTablet && 'isTablet',
+            device.isDesktop && 'isDesktop',
+          )}
+        >
+          <Header />
+          <main className="router-context">
+            <Router />
+          </main>
+        </div>
+        <Footer />
+        <div id="dynamic-portals" />
+      </>
+    );
+  }, [section]);
 }
 
 export default hot(App);
