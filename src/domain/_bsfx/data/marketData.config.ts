3851a2466324e48b61e9ca5ebbce1c6068cd0522
konstantinos-hycm <konstantinos.symeou@hycm.com>
2020-12-10 09:15:42 +0200

    [domain] import fixes

--- src/domain/_bsfx/data/marketData.config.ts
+++ src/domain/_bsfx/data/marketData.config.ts
@@ -1,6 +1,6 @@
 import { MarketType } from '@domain/enums';
-import { marketLeverages as _marketLeverages } from '../../_default/portal/market.config';
-export { marketTableContent } from '../../_default/portal/market.config';
+import { marketLeverages as _marketLeverages } from '../../_default/data/marketData.config';
+export { marketTableContent } from '../../_default/data/marketData.config';
 
 export const marketLeverages = {
   ..._marketLeverages,
