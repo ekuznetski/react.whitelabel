b7829bf1f28a87342389a0d373d367d95334fde2
konstantinos-hycm <konstantinos.symeou@hycm.com>
2020-12-10 09:18:59 +0200

    [domain] download links config

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -2,6 +2,6 @@ export * from './_default/routers.config';
 export * from './_default/locales.config';
 export * from './_default/validation.config';
 export * from './_default/data/marketData.config';
-export * from './_default/data/download-links.config';
+export * from './_default/data/downloadLinks.config';
 export * from './_default/data/profileMenu.config';
 export * from './_default/data/questions.config';
