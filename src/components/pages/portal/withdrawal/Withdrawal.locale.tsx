cb657700cd40f09585545237ab8da658855af3a9
MisaGu <aslebed@yahoo.com>
2020-12-11 15:57:44 +0200

    [withdrawal] add locale file

---
+++ src/components/pages/portal/withdrawal/Withdrawal.locale.tsx
@@ -0,0 +1,17 @@
+import i18n from 'i18next';
+import React from 'react';
+import { Trans } from 'react-i18next';
+const t = i18n.getFixedT(i18n.language);
+
+export const locale = {
+  withdrawalPolicyDesc: t('Withdrawal Policy Desc'),
+  isDormantAlert: (
+    <Trans i18nKey="90 inactive days warning">
+      You have been inactive for more than 90 days. Please contact us at{' '}
+      <a className="a-link" href="mailto:accounts@hycm.com">
+        accounts@hycm.com
+      </a>{' '}
+      to help you re-activate your account
+    </Trans>
+  ),
+};
