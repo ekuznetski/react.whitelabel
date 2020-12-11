d32892519561e6972dbf96f403b67f962c35e973
MisaGu <aslebed@yahoo.com>
2020-12-11 17:34:00 +0200

    [core] fix Footer & Header styles

--- src/components/core/header/auth/HeaderAuth.tsx
+++ src/components/core/header/auth/HeaderAuth.tsx
@@ -1,4 +1,4 @@
-import { Svg } from '@components/shared';
+import { LocaleLink, Svg } from '@components/shared';
 import { ELabels } from '@domain/enums';
 import { IHeaderDefaultProps } from '@domain/interfaces';
 import classNames from 'classnames';
@@ -10,10 +10,10 @@ export function HeaderAuth(props: IHeaderDefaultProps) {
   return (
     <div className={classNames('panel-menu', props.fixed && 'fixed')}>
       <Container>
-        <div className="logo">
+        <LocaleLink to="" className="logo">
           <Svg className="m-auto" href="logo" _label />
           <Svg className="mx-auto mb-n5" href="logo" _label={ELabels.bsfx} height={58} />
-        </div>
+        </LocaleLink>
       </Container>
     </div>
   );
