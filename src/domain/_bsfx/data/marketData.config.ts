fa2e267448bf160138e90a1cc45df544e5574aa9
konstantinos-hycm <konstantinos.symeou@hycm.com>
2020-12-11 16:09:20 +0200

    [domain] merge fixes

--- src/domain/_bsfx/data/marketData.config.ts
+++ src/domain/_bsfx/data/marketData.config.ts
@@ -1,8 +1,20 @@
-import { MarketType } from '@domain/enums';
+import { ETradingPlatform, MarketType } from '@domain/enums';
 import { marketLeverages as _marketLeverages } from '@domain/data/marketData.config';
-export { marketTableContent } from '@domain/data/marketData.config';
+import { marketTableContent as _marketTableContent } from '@domain/data/marketData.config';
 
 export const marketLeverages = {
   ..._marketLeverages,
   [MarketType.forex]: '1:500',
+};
+
+const tableEntries = Object.entries(_marketTableContent).map(([key, value]) => [
+  key,
+  value.map((item) => ({ ...item, raw: null, platform: [ETradingPlatform.mt5] })),
+]);
+
+export const marketTableContent = Object.fromEntries(tableEntries);
+
+export const marketFiles = {
+  financeFeesFixed: 'https://www.bluesquarefx.com/files/FINANCE_FEES_MT5_FIXED.pdf',
+  financeFeesVariable: 'https://www.bluesquarefx.com/files/FINANCE_FEES_MT5_VARIABLE.pdf',
 };
