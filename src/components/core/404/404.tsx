d2087aeed5af94638d48371861ba558feb5d3334
konstantinos-hycm <konstantinos.symeou@hycm.com>
2020-12-11 14:04:39 +0200

    [core] 404 page

--- src/components/core/404/404.tsx
+++ src/components/core/404/404.tsx
@@ -1,5 +1,26 @@
+import { Button, LocaleLink, SectionBg } from '@components/shared';
 import React, { memo } from 'react';
+import { useTranslation } from 'react-i18next';
+import './404.scss';
 
 export const NotFound = memo(function NotFoundPage() {
-  return <div>404</div>;
+  const { t } = useTranslation();
+
+  return (
+    <section className="notFoundPage">
+      <SectionBg img="404-bg.jpg" />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 col-lg-7 notFoundPage__header">
+            <div className="notFoundPage__text-title mb-7">404</div>
+            <div className="notFoundPage__text-subTitle mb-7">{t('Not Found')}</div>
+            <div className="notFoundPage__text-description mb-9">{t('Not Found Page desc')}</div>
+            <Button className="notFoundPage__register-btn">
+              <LocaleLink to="/">{t('Go to Home Page')}</LocaleLink>
+            </Button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 });
