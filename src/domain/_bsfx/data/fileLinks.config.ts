d60c6fc0ef349587b6913894337b773e5159ad95
konstantinos-hycm <konstantinos.symeou@hycm.com>
2020-12-15 09:34:11 +0200

    [files] added download_mt5 to fileLinks

--- src/domain/_bsfx/data/fileLinks.config.ts
+++ src/domain/_bsfx/data/fileLinks.config.ts
@@ -2,3 +2,8 @@ export const marketFiles = {
   financeFeesFixed: '/files/finance_fees_mt5_fixed.pdf',
   financeFeesVariable: '/files/finance_fees_mt5_variable.pdf',
 };
+
+export const platformFiles = {
+  downloadMT4: '',
+  downloadMT5: '/files/download_mt5.pdf',
+};
--- src/domain/_default/data/fileLinks.config.ts
+++ src/domain/_default/data/fileLinks.config.ts
@@ -2,3 +2,8 @@ export const marketFiles = {
   financeFeesFixed: '',
   financeFeesVariable: '',
 };
+
+export const platformFiles = {
+  downloadMT4: '',
+  downloadMT5: '',
+};
