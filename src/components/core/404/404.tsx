c1da82be6bfdc462cef8b60e90bee7e60ee34281
konstantinos-hycm <konstantinos.symeou@hycm.com>
2020-12-15 15:29:46 +0200

    [core] 404 style updates

--- src/components/core/404/404.tsx
+++ src/components/core/404/404.tsx
@@ -16,16 +16,16 @@ export const NotFound = memo(function NotFound() {
     <section className="not-found">
       <Container className="align-self-center">
         <div className="row">
-          <div className=" m-auto col-12 not-found__header">
+          <div className=" m-auto col-12 not-found__header mt-n2">
             <div className="not-found__title mb-12">404</div>
             <div className="not-found__subTitle mb-7">{t('Not Found')}</div>
-            <div className="not-found__description mb-9">{t('Not Found Page desc')}</div>
+            <div className="not-found__description mb-13">{t('Not Found Page desc')}</div>
             <div className="not-found__btns">
-              <Button className="mx-7 mt-3">
+              <Button className="mx-7 mt-3 col-12 col-sm-6 col-md-4 col-lg-3">
                 <LocaleLink to="">{t('Go to Home Page')}</LocaleLink>
               </Button>
               {!!clientProfile && (
-                <Button className="mx-7 my-3">
+                <Button className="mx-7 mt-3 col-12 col-sm-6 col-md-4 col-lg-3">
                   <LocaleLink to="/dashboard">{t('Go to Dashboard')}</LocaleLink>
                 </Button>
               )}
