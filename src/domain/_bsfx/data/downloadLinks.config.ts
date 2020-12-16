f07b8a04444d6ba13e10de9810df6fbefd053a20
ekuznetski <e.kuznetski@gmail.com>
2020-12-16 17:56:59 +0200

    [portal] fix downloadLinks.config

--- src/domain/_bsfx/data/downloadLinks.config.ts
+++ src/domain/_bsfx/data/downloadLinks.config.ts
@@ -6,7 +6,6 @@ export const downloadLinks = {
     desktop: 'https://download.mql5.com/cdn/web/17115/mt5/bluesquare5setup.exe',
     appStore: 'https://apps.apple.com/us/app/metatrader-5/id413251709',
     googlePlay: 'https://play.google.com/store/apps/details?id=net.metaquotes.metatrader5&hl=en_US&gl=US',
-    desktop: 'https://download.mql5.com/cdn/web/17115/mt5/bluesquare5setup.exe',
     web: getMetaTraderWebTerminalLink({
       version: 5,
       servers: ['BlueSquare-Live'],
