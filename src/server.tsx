ca6fd28cd0f7493ecff8a1df479357e95de409b2
konstantinos-hycm <konstantinos.symeou@hycm.com>
2020-12-17 12:09:44 +0200

    [server] added protocol to url

--- src/server.tsx
+++ src/server.tsx
@@ -146,8 +146,8 @@ app.get('*', (req: express.Request, res: express.Response) => {
             <meta property="og:type" content="website">
             <meta property="og:title" content="${route?.meta.title}">
             <meta property="og:description" content="${route?.meta.desc}">
-            <meta property="og:image" content="${req.headers.host}/assets/og-img.png">
-            <meta property="og:url" content="${req.headers.host}">
+            <meta property="og:image" content="https://${req.headers.host}/assets/og-img.png">
+            <meta property="og:url" content="https://${req.headers.host}">
             <meta name="twitter:card" content="summary_large_image">`,
           ),
       );
