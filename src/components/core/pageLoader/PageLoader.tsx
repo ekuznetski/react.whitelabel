984b4e9797769839d87daac456d2b728dc01364d
MisaGu <aslebed@yahoo.com>
2020-12-17 14:18:49 +0200

    [router] fix loading effect

--- src/components/core/pageLoader/PageLoader.tsx
+++ src/components/core/pageLoader/PageLoader.tsx
@@ -4,13 +4,13 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import './PageLoader.scss';
 
-export function PageLoader({ isLoading }: any) {
+export function PageLoader({ isLoading, overlay = false }: any) {
   const elemRef = React.createRef<HTMLDivElement>();
   const { t } = useTranslation();
 
   return (
-    <div className={classNames('page-loader', !isLoading && 'hide')} ref={elemRef}>
-      <Img src="loader.gif" />
+    <div className={classNames('page-loader', !isLoading && 'hide', overlay && 'overlay')} ref={elemRef}>
+      {!overlay && <Img src="loader.gif" />}
       {/* {t('Page loading')} */}
     </div>
   );
