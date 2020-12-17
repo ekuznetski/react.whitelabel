3ac8b7a8c6cf32c4c5354122a0600abc75fa3ab6
konstantinos-hycm <konstantinos.symeou@hycm.com>
2020-12-17 17:36:12 +0200

    [pages] products pages & config changes

--- src/components/pages/main/products/_bsfx/Products.config.tsx
+++ src/components/pages/main/products/_bsfx/Products.config.tsx
@@ -9,7 +9,34 @@ const t = i18n.getFixedT(i18n.language);
 export const config = {
   ..._config,
   tableSections: _config.tableSections.map((section) => {
-    if (section.id === MarketType.crypto) {
+    if (section.id === MarketType.forex) {
+      return {
+        ...section,
+        points: [
+          <Trans i18nKey="Spreads from # pips" values={{ val: '1.4' }}>
+            Spreads from <b>1.4 pips</b>
+          </Trans>,
+          <Trans i18nKey="Max Leverage #" values={{ val: '1:500' }}>
+            Max. Leverage <b>1:500</b>
+          </Trans>,
+          <Trans i18nKey="Margins from just #" values={{ val: '0.20%' }}>
+            Margins from just <b>0.20%</b>
+          </Trans>,
+        ],
+      };
+    } else if (section.id === MarketType.indices) {
+      return {
+        ...section,
+        points: [
+          <>
+            <b>15+</b> {t('Most Popular Indices Worldwide')}
+          </>,
+          <Trans i18nKey="Max Leverage #" values={{ val: '1:100' }}>
+            Max. Leverage <b>1:100</b>
+          </Trans>,
+        ],
+      };
+    } else if (section.id === MarketType.crypto) {
       return {
         ...section,
         points: [
@@ -19,6 +46,15 @@ export const config = {
           <>
             <b>60+</b> {t('Cryptocurrencies')}
           </>,
+        ],
+      };
+    } else if (section.id === MarketType.commodities) {
+      return {
+        ...section,
+        points: [
+          <Trans i18nKey="Max Leverage #" values={{ val: '1:133' }}>
+            Max. Leverage <b>1:67</b>
+          </Trans>,
         ],
       };
     } else {
