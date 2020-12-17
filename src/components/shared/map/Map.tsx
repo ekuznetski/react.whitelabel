6899fdec73731bb6ff0c2a77695740339431b924
konstantinos-hycm <konstantinos.symeou@hycm.com>
2020-12-17 17:38:45 +0200

    [shared] map types fix & key moved to env

--- src/components/shared/map/Map.tsx
+++ src/components/shared/map/Map.tsx
@@ -1,13 +1,14 @@
+import { env } from '@env';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 import React, { memo, useState } from 'react';
 import { IMap, mapStyles } from './map-context';
 
-export const Map = memo(({ defaultZoom = 15, defaultCenter, height = '500px', width = '100%', markers = [] }: IMap) => {
+export const Map = memo(({ defaultZoom = 15, defaultCenter, height = '500px', width = '100%', markers }: IMap) => {
   const [map, setMap] = useState({});
   const [centerChanged, setCenterChanged] = useState(false);
 
   return (
-    <LoadScript googleMapsApiKey="AIzaSyCvXa4VevmTmTayzh4EB4n22Hs769ffr_U">
+    <LoadScript googleMapsApiKey={env.GOOGLE_MAP_KEY}>
       <GoogleMap
         mapContainerStyle={{ height, width }}
         center={defaultCenter}
--- src/env/_bsfx/env.config.ts
+++ src/env/_bsfx/env.config.ts
@@ -4,6 +4,7 @@ const dev = {
   GTM_ID: 'GTM-KWJHRK9',
   INTERCOM_ID: 'p31288aj',
   API_URL: 'https://api.bluesquarefx.com',
+  GOOGLE_MAP_KEY: 'AIzaSyCvXa4VevmTmTayzh4EB4n22Hs769ffr_U',
 };
 
 const prod = {
