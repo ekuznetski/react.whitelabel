22a592bd9e661fb6b73eeab68a4e42703bee2bd2
konstantinos-hycm <konstantinos.symeou@hycm.com>
2020-12-28 10:54:00 +0200

    [locale] title changes

--- src/locale/_bsfx/pagesMeta.js
+++ src/locale/_bsfx/pagesMeta.js
@@ -5,7 +5,7 @@ export default {
       'Blue Square FX is a leading provider of online foreign exchange (FX) trading. Blue Square FX provides a quicker, simpler, cleaner system, designed by FX traders for FX traders, to ensure an excellent user experience.',
   },
   About: {
-    title: 'About us - Blue Square FX',
+    title: 'Blue Square FX - Build by FX traders, for FX traders.',
     desc:
       'Blue Square FX is a leading provider of online foreign exchange (FX) trading. Blue Square FX provides a quicker, simpler, cleaner system, designed by FX traders for FX traders, to ensure an excellent user experience.',
   },
@@ -15,7 +15,7 @@ export default {
       'Blue Square FX is a leading provider of online foreign exchange (FX) trading. Blue Square FX provides a quicker, simpler, cleaner system, designed by FX traders for FX traders, to ensure an excellent user experience.',
   },
   Platform: {
-    title: 'Platforms - Blue Square FX',
+    title: 'Platform - Blue Square FX',
     desc:
       'Blue Square FX’s Meta Trader technology is for traders of all levels and expertise, offering flexible trading systems, a mobile app, Expert Advisors, and advanced technical analysis.',
   },
@@ -38,7 +38,7 @@ export default {
     title: 'Login - Blue Square FX',
   },
   Registration: {
-    title: 'Registration - Blue Square FX',
+    title: 'Blue Square FX - Build by FX traders, for FX traders.',
     desc:
       'Blue Square FX is a leading provider of online foreign exchange (FX) trading. Blue Square FX provides a quicker, simpler, cleaner system, designed by FX traders for FX traders, to ensure an excellent user experience.',
   },
