b952de1ff50ddcddc923af7f991bee0dc0d2ba5e
ekuznetski <e.kuznetski@gmail.com>
2020-12-28 17:29:06 +0200

    [modal] add new modal, managed via redux

--- src/components/core/index.ts
+++ src/components/core/index.ts
@@ -3,4 +3,5 @@ export * from './header/Header';
 export * from './router/Router';
 export * from './pageLoader/PageLoader';
 export * from './notification/Notification';
+export * from './modal/ModalNew';
 export * from './404/404';
---
+++ src/components/core/modal/ModalNew.tsx
@@ -0,0 +1,63 @@
+import { Svg } from '@components/shared';
+import { IModalState } from '@domain/interfaces';
+import { IStore, ac_hideModal } from '@store';
+import classNames from 'classnames';
+import React, { memo, useEffect } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import './Modal.scss';
+
+interface IModalStateProps {
+  modalProps: IModalState;
+}
+
+export const ModalNew = memo(function Modal() {
+  const { modalProps } = useSelector<IStore, IModalStateProps>((state) => ({
+    modalProps: state.app.modal,
+  }));
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    return () => {
+      dispatch(ac_hideModal());
+    };
+  }, []);
+
+  function closeModal(e: any) {
+    e.stopPropagation();
+    dispatch(ac_hideModal());
+  }
+
+  return modalProps.visible ? (
+    <div className={classNames('modal-wrapper', modalProps.modalWrapperClassName)}>
+      <div className="modal-overlay" onClick={closeModal} />
+      <div className="common-modal p-9">
+        <Svg href="close" className="common-modal__close" height={18} onClick={closeModal} />
+        <div className="common-modal__content">{React.createElement(modalProps.component, modalProps.props)}</div>
+      </div>
+    </div>
+  ) : null;
+});
+
+export function ModalTitle(props: {
+  className?: string;
+  children?: React.ReactNode;
+  title?: React.ReactNode;
+  subTitle?: React.ReactNode;
+}) {
+  return (
+    <div className={classNames('modal-content__title mb-5', props.className)}>
+      {props.title}
+      {props.subTitle && <small className="mt-1">{props.subTitle}</small>}
+      {props.children}
+    </div>
+  );
+}
+
+export function ModalBody(props: { className?: string; children: React.ReactNode }) {
+  return <div className={classNames('modal-content__body', props.className)}>{props.children}</div>;
+}
+
+export function ModalFooter(props: { className?: string; children: React.ReactNode }) {
+  return <div className={classNames('modal-content__footer', props.className)}>{props.children}</div>;
+}
