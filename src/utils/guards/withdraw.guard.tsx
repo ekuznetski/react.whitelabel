7503fc6e3080263f8e641f8e53489c159b64ef55
ekuznetski <e.kuznetski@gmail.com>
2021-01-25 13:06:29 +0200

    [portal] check flags for allowing additional accounts on dashboard

--- src/utils/guards/withdraw.guard.tsx
+++ src/utils/guards/withdraw.guard.tsx
@@ -2,5 +2,5 @@ import { IRouteGuard } from '@domain/interfaces';
 import { store } from '@store';
 
 export function withdrawGuard(): IRouteGuard {
-  return store.getState().data.client.settings.allow_withdrawal;
+  return !!store.getState().data.client?.settings.allow_withdrawal;
 }
