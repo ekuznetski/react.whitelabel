c80c46920c6ed39b1858a2888c4a67d6ef346093
MisaGu <aslebed@yahoo.com>
2021-02-03 16:57:27 +0200

    fix cors on live - test 11

--- src/server.tsx
+++ src/server.tsx
@@ -202,7 +202,6 @@ app.use('/proxy', checkAuthenticationCookie, (req, resp) => {
 
       resp.set(res.headers);
       resp.setHeader('Access-Control-Allow-Origin', 'http://18.133.160.155:3000');
-      res.setHeader('Access-Control-Allow-Credentials', true);
       return resp.status(res.status).send(res.data);
     })
     .catch((err) => {
