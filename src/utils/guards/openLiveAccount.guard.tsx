7c2a2bed05d0f579a008007277dacb4b1a3f2455
MisaGu <aslebed@yahoo.com>
2021-02-12 10:29:25 +0200

    [guard] openLiveAccountGuard

--- src/utils/guards/openLiveAccount.guard.tsx
+++ src/utils/guards/openLiveAccount.guard.tsx
@@ -1,6 +1,13 @@
-import { IRouteGuard } from '@domain/interfaces';
+import { EPagePath } from '@domain/enums';
+import { IActivatorProps, IRouteGuard } from '@domain/interfaces';
 import { store } from '@store';
 
-export function openLiveAccountGuard(): IRouteGuard {
-  return !!store.getState().data.client?.settings?.allow_additional_live_account;
+export function openLiveAccountGuard({ routeState }: IActivatorProps): IRouteGuard {
+  const _allowed = !!store.getState().data.client?.settings?.allow_additional_live_account;
+  return _allowed || !!routeState?.prev?.path
+    ? false
+    : {
+        path: EPagePath.Dashboard,
+        state: { from: location.pathname },
+      };
 }
