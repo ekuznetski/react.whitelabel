1715c5da2d2553f1290eaa8255a8fa852cb0c729
MisaGu <aslebed@yahoo.com>
2021-02-16 10:00:10 +0200

    update prices only when user on Home or Products pages

--- src/webSocket.tsx
+++ src/webSocket.tsx
@@ -1,24 +1,28 @@
-import { EWebSocketMessage, MarketType } from '@domain/enums';
-import { ac_savePrices } from '@store';
+import { EPagePath, EWebSocketMessage, MarketType } from '@domain/enums';
+import { IAppStore, IStore, ac_savePrices } from '@store';
 import { memo, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import socket from 'socket.io-client';
 
 const io = socket();
 
 export const WebSocketListener = memo(function WebSocketListener() {
+  const { route } = useSelector<IStore, { route: IAppStore['route'] }>((state) => ({
+    route: state.app.route,
+  }));
   const dispatch = useDispatch();
-  
+
   useEffect(() => {
     io.on(EWebSocketMessage.prices, (pricesData: any) => {
-      dispatch(
-        ac_savePrices(
-          Object.keys(pricesData).reduce((acc, e) => {
-            Object.assign(acc, { [e.toLowerCase() as keyof typeof MarketType]: pricesData[e] });
-            return acc;
-          }, {}),
-        ),
-      );
+      if ([EPagePath.Home, EPagePath.Products].includes(route.path))
+        dispatch(
+          ac_savePrices(
+            Object.keys(pricesData).reduce((acc, e) => {
+              Object.assign(acc, { [e.toLowerCase() as keyof typeof MarketType]: pricesData[e] });
+              return acc;
+            }, {}),
+          ),
+        );
     });
   });
 
