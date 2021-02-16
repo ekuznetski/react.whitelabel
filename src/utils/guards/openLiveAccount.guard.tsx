0725d490e6b7b39bd694709aa941229e488382dd
MisaGu <aslebed@yahoo.com>
2021-02-16 11:17:07 +0200

    fix

--- src/utils/guards/openLiveAccount.guard.tsx
+++ src/utils/guards/openLiveAccount.guard.tsx
@@ -3,7 +3,7 @@ import { IActivatorProps, IRouteGuard } from '@domain/interfaces';
 import { store } from '@store';
 
 export function openLiveAccountGuard({ routeState }: IActivatorProps): IRouteGuard {
-  const _allowed = !!store.getState().data.client?.settings?.allow_additional_live_account;
+  const _allowed = store.getState().data.client?.settings?.allow_additional_live_account;
   return _allowed
     ? _allowed
     : !!routeState?.prev?.path
