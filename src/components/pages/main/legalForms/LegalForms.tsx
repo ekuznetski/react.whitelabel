41e0360da9a0e1616ea385ff5f8bc062d8101a8f
ekuznetski <e.kuznetski@gmail.com>
2021-03-11 16:48:29 +0200

    [portal] legal forms update to use cards

--- src/components/pages/main/legalForms/LegalForms.tsx
+++ src/components/pages/main/legalForms/LegalForms.tsx
@@ -2,35 +2,24 @@ import React from 'react';
 
 import { useTranslation } from 'react-i18next';
 import { SimplePageTopSection } from '@sections/simplePageTopSection/SimplePageTopSection';
-import { BackButton, Col, Row, Svg } from '@components/shared';
+import { BackButton, Cards, Col, Row, Svg } from '@components/shared';
 import { config } from './LegalForms.config';
 import './LegalForms.scss';
 
 export function LegalForms() {
   const { t } = useTranslation();
-
+  const cardsConfig = config.documents.map((e, i) => {
+    return {
+      header: <CardHeader title={e.title} />,
+      content: <CardContent link={e.link} />,
+      uid: i + 1,
+    };
+  });
   return (
     <>
       <SimplePageTopSection title={t('Legal Forms and Documents')} />
       <div className="legal-forms-wrapper container">
-        <Row>
-          {config.documents.map((doc) => (
-            <Col key={doc.title} xs="12" sm="6">
-              <div className="document-wrapper">
-                <div className="document-title">
-                  <div className="document-icon">
-                    <Svg href="document" height="20" width="16" />
-                  </div>
-                  {doc.title}
-                </div>
-                <a className="document-button" download href={doc.link}>
-                  <Svg href="download" height="16" width="16" />
-                  {t('Download')}
-                </a>
-              </div>
-            </Col>
-          ))}
-        </Row>
+        <Cards id="legalCards" className="row" cardWrapperClass="col-xs-12 col-sm-6 mb-9" cards={cardsConfig} />
         <Row className="mb-5 d-lg-none">
           <Col>
             <BackButton />
@@ -38,5 +27,27 @@ export function LegalForms() {
         </Row>
       </div>
     </>
+  );
+}
+
+function CardHeader({ title }: { title: string }) {
+  return (
+    <div className="document-title">
+      <div className="document-icon">
+        <Svg href="document" height="20" width="16" />
+      </div>
+      {title}
+    </div>
+  );
+}
+
+function CardContent({ link }: { link: string }) {
+  const { t } = useTranslation();
+
+  return (
+    <a className="document-button" download href={link}>
+      <Svg href="download" height="16" width="16" />
+      {t('Download')}
+    </a>
   );
 }
